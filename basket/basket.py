399823527355d70babed9f318da312cf6da99821
SebastianMou <Sebastian_Mou@protonmail.com>
2021-09-27 23:52:19 -0500

    Error "type object 'Product' has no attribute 'products' " - summary.html

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,5 @@
-
+from store.models import Product
+from decimal import Decimal
 
 class Basket():
     """"
@@ -14,13 +15,39 @@ class Basket():
             basket = self.session['skey'] = {}  ## set up new session
         self.basket = basket                    ## contaning new session or old session
 
-    def add(self, product):
+    def add(self, product, qty):
         """
         adding and updating the user basket session data
         """
-        product_id = product.id
+        product_id = product.id                                                             ## save product_id in product.id
 
-        if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
+        ## the self.basket wich can acces the info and the basket can have informacion about the users session
+        if product_id not in self.basket:                                                   ## if product_iud is not in the basket
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}        ## is no session id exist it will create session
 
-        self.session.modified = True
+        self.session.modified = True                                                        ## telling django that we have modified the session
+
+    def __iter__(self):
+        """
+        collect the product in the session data to query database and return products
+        """
+        product_ids = self.basket.keys()                        ## collect ALL the keys/products
+        products = Product.products.filter(id__in=product_ids) 
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+        
+
+    def __len__(self):
+        """
+        get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
--- basket/views.py
+++ basket/views.py
@@ -1,23 +1,30 @@
 from django.http import response
 from django.http.response import Http404, JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+
 from store.models import Product 
 
 from .basket import Basket
 
 # Create your views here.
-
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
+
 
 def basket_add(request):
-    basket = Basket(request)
-    if request.POST.get('action') == 'post':
-        product_id = int(request.POST.get('productid'))
-        try:
-            product = Product.objects.get(id=product_id)
+    """
+    colect data that was sent from js/ajaxs/ detail.html
+    """
+    basket = Basket(request)                                ## grab the session data
+    if request.POST.get('action') == 'post':                ## seeing if the request is a post request
+        product_id = int(request.POST.get('productid'))     ## get product id
+        ##product_qty = int(request.POST.get('productqty'))   ## get product qty
+        try:                                                ## exeption 
+            product = Product.objects.get(id=product_id)    ## get product_id
         except:
             raise Http404
-        basket.add(product=product)
-        response = JsonResponse({'text':'data'})
+        basket.add(product=product, qty=1)                  ## save to session
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})           ## make jasonresponse / to see in console
         return response
